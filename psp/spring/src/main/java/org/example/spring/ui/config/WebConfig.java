451e76e998b46a8c413e6d2ab994984ca61eada1
oscar <oscar@oscar.com>
2025-12-09 08:56:30 +0100

    registro interceptor

---
+++ psp/spring/src/main/java/org/example/spring/ui/config/WebConfig.java
@@ -0,0 +1,25 @@
+package org.example.spring.ui.config;
+
+
+import org.example.spring.ui.interceptor.AuthInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthInterceptor authInterceptor;
+
+    public WebConfig(AuthInterceptor authInterceptor) {
+        this.authInterceptor = authInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor)
+                .addPathPatterns("/api/**");
+    }
+
+
+}
+
