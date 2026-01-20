18b80f48c7a972fed4ad34a649b56cf2ff586d8a
oscarnovillo <oscar.novillo@gmail.com>
2026-01-20 14:00:58 +0100

    crypto

---
+++ psp/SpringCrypto/src/main/java/org/example/springcrypto/SpringCryptoApplication.java
@@ -0,0 +1,13 @@
+package org.example.springcrypto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCryptoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCryptoApplication.class, args);
+    }
+
+}
